e2f0b4f78d137b8c84cc4094be8f6215de1a5653
Kris <KrisCaseyBiotech@gmail.com>
2020-03-23 04:15:15 +0000

    Add 3 more questions

--- job_search_log.py
+++ job_search_log.py
@@ -1,13 +1,35 @@
 from __future__ import print_function, unicode_literals
 from PyInquirer import prompt, print_json
 
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+
+from clint.textui import colored
+
 questions = [
     {
         'type': 'input',
-        'title': 'job_title',
-        'message': 'What is the title of the job',
-    }
+        'name': 'job_title',
+        'message': 'What is the title of the job:',
+    },
+    {
+        'type': 'input',
+        'name': 'job_description',
+        'message': 'What is the job description:',
+    },
+    {
+        'type': 'input',
+        'name': 'cover_letter',
+        'message': 'Enter the cover letter written for this job:'
+    },
+    {
+        'type': 'input',
+        'name': 'application_date',
+        'message': 'Enter the application date:'
+    }        
 ]
 
 answers = prompt(questions)
-print_json(answers)
+print(answers)
